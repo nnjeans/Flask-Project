28e8352b39baa022164cca449bc8b78adc65aef8
perc <bossaboybeats@gmail.com>
2025-11-07 10:20:13 -0800

    Flask App V1P
    iteration

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+@app.route("/testing")
+def testing():
+    return "<h1>Testing Endpoint</h1>"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000, host='0.0.0.0')
